2681d12f78f1279b376d05965e56c275c50ecbe9
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-02-29 22:56:28 +0000

    Adding sound effects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 
 pygame.font.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))                                          # Setting the width and height variables for screen size.
@@ -14,6 +15,8 @@ YELLOW = (0,255,255)
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)                                      # Creating a border in the center of the screen.
 
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join("Assets", "Grenade+1.mp3"))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join("Assets", "Gun+Silencer.mp3"))
 
 HEALTH_FONT = pygame.font.SysFont("comicsans", 40)
 WINNER_FONT = pygame.font.SysFont("comicsans", 100)
@@ -127,16 +130,20 @@ def main():                                                                     
                 if event.key == pygame.K_LALT and len(yellow_bullets) < MAX_BULLETS:      # Creating and centering the bullet onto the yellow spaceship.
                     bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
                 
                 
                 if event.key == pygame.K_RALT and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x, red.y + red.height//2 -2 , 10, 5)
                     red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
                 
             if event.type == RED_HIT:
                 red_health -= 1
+                BULLET_HIT_SOUND.play()
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
+                BULLET_HIT_SOUND.play()
         
         winner_text = ""
         if red_health <= 0:
