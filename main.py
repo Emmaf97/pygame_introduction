eed205a55bf0df3aab85f3f55610723074985032
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-02-29 15:28:22 +0000

    Drawing Spaceships v1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))                         # Setting the width and height variables for screen size.
@@ -8,12 +9,24 @@ WHITE = (255,255,255)                                                  # Setting
 
 FPS = 60
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
+                                                                       # The below handles getting file path without using slashes,
+                                                                       # in the event of different operating system
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join("Assets", "spaceship_red.png")) 
+                                                                       # Resizing the Image to fit the screen.
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT))
+
 def draw_window():                                                     # creating a draw method to draw objects onto the screen.
     WIN.fill((WHITE))
+    WIN.blit(YELLOW_SPACESHIP, (300,100))                              # Drawing the spaceship as a surface onto the screen
     pygame.display.update()
 
 
-def main():                                                            # setting the main game loop and cappoing the FPS to 60,
+def main():                                                            # setting the main game loop and capping the FPS to 60,
                                                                        # so that it will be stable on all machines.
     clock = pygame.time.Clock()
     run = True
